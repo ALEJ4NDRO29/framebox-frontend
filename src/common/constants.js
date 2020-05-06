d40d809a21ecaeea4340413c0f61c54b2381712c
Alejandro <josealejandro.r.29@gmail.com>
2020-05-06 19:10:05 +0200

    docker tests

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,6 @@
 export const VERSION = "0.1.0";
-export const FRAMEBOX_API_URL = 'http://192.168.1.50:3000/api/v1'
+// export const FRAMEBOX_API_URL = 'http://192.168.1.50:3000/api/v1'
+export const FRAMEBOX_API_URL = process.env.VUE_APP_API;
 
 export const BOOTSTRAP_PAGINATION_CLASSES = {
     ul: 'pagination',
--- src/i18n.js
+++ src/i18n.js
@@ -16,6 +16,9 @@ function loadLocaleMessages () {
   return messages
 }
 
+console.log(process.env.VUE_APP_API);
+console.log(process.env.VUE_APP_I18N_LOCALE);
+
 export default new VueI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
